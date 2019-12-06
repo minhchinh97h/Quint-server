d78595a398e0a836995ccf3190b5fbed9f096b85
minhchinh97h <minhchinhduong97@gmail.com>
2019-12-06 21:16:28 +0200

    Update .env for server's url to return verification link

--- app/actions/auth.js
+++ app/actions/auth.js
@@ -1,5 +1,3 @@
-const express = require("express");
-const path = require("path");
 const sgMail = require("@sendgrid/mail");
 const firebase_admin = require("firebase-admin");
 
@@ -21,7 +19,7 @@ exports._validatePassword = password => {
 // const logo_png = require("../../public/pngs/logo.png")
 
 exports._sendVerificationEmail = (email, uuid, token) => {
-  let link = `http://localhost:3000/auth?email=${email}&id=${uuid}&token=${token}`;
+  let link = `${process.env.SERVER_URL}auth?email=${email}&id=${uuid}&token=${token}`;
   let message = {
     to: email,
     from: "quintapp@gmail.com",
