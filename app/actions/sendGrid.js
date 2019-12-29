204879952443b8ce461f1acd57ac05f4ac29a6fd
minhchinh97h <minhchinhduong97@gmail.com>
2019-12-29 22:23:14 +0200

    Update sendGrid.js

--- app/actions/sendGrid.js
+++ app/actions/sendGrid.js
@@ -5,7 +5,7 @@ const _sendVerificationEmail = (email, uuid, token) => {
   let link = `${process.env.SERVER_URL}auth?action=verify&id=${uuid}&token=${token}`;
   let message = {
     to: email,
-    from: "noreply",
+    from: "noreply@quint-app.com",
     templateId: "d-5861fe691a85455196e36093a0ad8b9c",
     // subject: 'Testing email from Quint',
     // text: 'HI',
