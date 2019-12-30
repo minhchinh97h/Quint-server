93a677a5702aa9a42cf5cc85b70d831d91b4ced3
minhchinh97h <minhchinhduong97@gmail.com>
2019-12-31 00:14:51 +0200

    Update verifyEmail.js

--- app/middlewares/auth/verifyEmail.js
+++ app/middlewares/auth/verifyEmail.js
@@ -1,7 +1,7 @@
 const HELPERS = require("../../helpers");
 const ACTIONS = require("../../actions");
 const path = require("path");
-const firebase_admin = require("firebase-admin")
+const firebase_admin = require("firebase-admin");
 
 const _processEmailVerificationMDW = async (req, res, next) => {
   const { id, token } = req.query;
@@ -52,6 +52,10 @@ const _processEmailVerificationMDW = async (req, res, next) => {
 
       // If the token is still valid
       if (now - token_created_at < expire_time) {
+        // 1 month of beta testing, in this time, users who register new accounts will have access to Premium content.
+        let start_beta_testing_timestamp = new Date(2019, 11, 31).getTime(),
+          end_beta_testing_timestamp = new Date(2020, 0, 31).getTime();
+
         // Check if the used_referral_code is valid
         let used_referral_code = get_user_db_response.data()
             .usedReferralCodeData.value,
@@ -78,30 +82,38 @@ const _processEmailVerificationMDW = async (req, res, next) => {
             // If the refer's uuid is valid, we will grant the refer and referred 30 days premium
             if (refer_uuid.length > 0) {
               // We have to check if the expiry timestamp of the refer is past date or not
-              let [get_refer_user_response, get_refer_user_error] = await HELPERS.promise._handlePromise(ACTIONS.users._getUser(refer_uuid))
+              let [
+                get_refer_user_response,
+                get_refer_user_error
+              ] = await HELPERS.promise._handlePromise(
+                ACTIONS.users._getUser(refer_uuid)
+              );
 
-              if(get_refer_user_error){
-                res.send(get_refer_user_error)
-                return
+              if (get_refer_user_error) {
+                res.send(get_refer_user_error);
+                return;
               }
 
-              let refer_expiry_timestamp = get_refer_user_response.data().expiryTimestamp,
-              update_refer_data = {
-                uuid: refer_uuid,
-                "package.plan": "premium"
-              }
-              
+              let refer_expiry_timestamp = get_refer_user_response.data()
+                  .expiryTimestamp,
+                update_refer_data = {
+                  uuid: refer_uuid,
+                  "package.plan": "premium"
+                };
+
               let extra_time = 30 * 24 * 60 * 60 * 1000,
                 timestamp = Date.now(),
                 referred_expiry_timestamp = timestamp + extra_time;
 
               // If refer's expiry timestamp is past date, we take current timestamp as base
-              if(refer_expiry_timestamp < Date.now()){
-                update_refer_data["expiryTimestamp"] = Date.now() + extra_time
-              } 
+              if (refer_expiry_timestamp < Date.now()) {
+                update_refer_data["expiryTimestamp"] = Date.now() + extra_time;
+              }
               // If not, we take the expiry timestamp as base
-              else{
-                update_refer_data["expiryTimestamp"] = firebase_admin.firestore.FieldValue.increment(extra_time)
+              else {
+                update_refer_data[
+                  "expiryTimestamp"
+                ] = firebase_admin.firestore.FieldValue.increment(extra_time);
               }
 
               let promises = [
@@ -155,6 +167,7 @@ const _processEmailVerificationMDW = async (req, res, next) => {
             // If the refer's uuid is not valid, we will not grant the referred nor the refer
             else {
               let timestamp = Date.now();
+
               let promises = [
                 // Update user in Auth
                 ACTIONS.auth._updateUserAuth(id, { emailVerified: true }),
@@ -162,7 +175,16 @@ const _processEmailVerificationMDW = async (req, res, next) => {
                 ACTIONS.users._updateUser({
                   uuid: id,
                   emailVerified: true,
-                  expiryTimestamp: timestamp,
+                  expiryTimestamp:
+                    Date.now() >= start_beta_testing_timestamp &&
+                    Date.now() <= end_beta_testing_timestamp
+                      ? end_beta_testing_timestamp
+                      : timestamp,
+                  // "package.plan":
+                  //   Date.now() >= start_beta_testing_timestamp &&
+                  //   Date.now() <= end_beta_testing_timestamp
+                  //     ? "premium"
+                  //     : "free",
                   "package.plan": "free",
                   "package.renewalTimestamp": timestamp
                 }),
@@ -195,6 +217,11 @@ const _processEmailVerificationMDW = async (req, res, next) => {
                 uuid: id,
                 emailVerified: true,
                 expiryTimestamp: timestamp,
+                // "package.plan":
+                //   Date.now() >= start_beta_testing_timestamp &&
+                //   Date.now() <= end_beta_testing_timestamp
+                //     ? "premium"
+                //     : "free",
                 "package.plan": "free",
                 "package.renewalTimestamp": timestamp
               }),
@@ -228,6 +255,11 @@ const _processEmailVerificationMDW = async (req, res, next) => {
               uuid: id,
               emailVerified: true,
               expiryTimestamp: timestamp,
+              // "package.plan":
+              //   Date.now() >= start_beta_testing_timestamp &&
+              //   Date.now() <= end_beta_testing_timestamp
+              //     ? "premium"
+              //     : "free",
               "package.plan": "free",
               "package.renewalTimestamp": timestamp
             }),
